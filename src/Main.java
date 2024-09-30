b0dd74458f0a2fc8a64971408c23469597d6a2cc
Anna Cardinal <anna.cardinal1717@gmail.com>
2024-09-30 13:39:43 -0700

    Practice

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String userName = "Anna";
+        String passWord = "1234";
+        String email = "Flying.Frisby12@yahoo.com";
+        String birthday = "12/14/1990";
+        int followers = 123456;
+        boolean isLoggedin;
+
+
+        System.out.println("Welcome to Tiktok would you like to log in?");
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Please enter your user name:" );
+        String userNames = myScanner.nextLine();
+        System.out.println("Please enter your password:");
+        String Passwords = myScanner.nextLine();
+        System.out.println("Welcome back " + userNames + "!");
+        if (Passwords.equals(passWord) && userNames.equals(userName)) {
+            System.out.println("Logged in!");
+                isLoggedin = true;}
+        else {
+            System.out.println("Password Incorrect.");
+                isLoggedin = false;
+        }
+    }
+}
